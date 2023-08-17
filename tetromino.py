a0e1befa63055a32a70b647fefe66acc8732f0c9
Your Name <you@example.com>
2023-08-17 10:43:23 +0530

    Code to display various tetrominoes

--- tetromino.py
+++ tetromino.py
@@ -1,13 +1,24 @@
 from settings import *
+import random
 
 
-class Block:
-    pass
+class Block(pg.sprite.Sprite):
+    def __init__(self, tetromino, pos):
+        self.tetromino = tetromino
+        self.pos = vec(pos) + INIT_POS_OFFSET
+        super().__init__(tetromino.tetris.sprite_group)
+        self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
+        self.image.fill('orange')
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.pos * TILE_SIZE
 
 
 class Tetromino:
     def __init__(self, tetris):
         self.tetris = tetris
+        self.shape = random.choice(list(TETROMINOES.keys()))
+        self.blocks = [Block(self, pos) for pos in TETROMINOES[self.shape]]
 
     def update(self):
         pass
