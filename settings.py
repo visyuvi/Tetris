2ef21b6d475eb843b02f0967b669c1937627cf01
Your Name <you@example.com>
2023-08-17 11:19:21 +0530

    Code to move tetrominoes down. Added controls for left and right movement

--- settings.py
+++ settings.py
@@ -5,11 +5,14 @@ vec = pg.math.Vector2
 FPS = 60
 FIELD_COLOR = (48, 39, 32)
 
+ANIM_TIME_INTERVAL = 500  # milliseconds
+
 TILE_SIZE = 50
 FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
 FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
 
-INIT_POS_OFFSET = vec(FIELD_SIZE) // 2
+INIT_POS_OFFSET = vec(FIELD_W  // 2 - 1, 0)
+MOVE_DIRECTIONS = {'left': vec(-1, 0), 'right': vec(1, 0), 'up': vec(0, -1), 'down': vec(0, 1)}
 TETROMINOES = {
     'T': [(0, 0), (-1, 0), (1, 0), (0, -1)],
     'O': [(0, 0), (0, -1), (1, 0), (1, -1)],
--- tetris.py
+++ tetris.py
@@ -9,13 +9,20 @@ class Tetris:
         self.sprite_group = pg.sprite.Group()
         self.tetromino = Tetromino(self)
 
+    def control(self, pressed_key):
+        if pressed_key == pg.K_LEFT:
+            self.tetromino.move(direction='left')
+        elif pressed_key == pg.K_RIGHT:
+            self.tetromino.move(direction='right')
+
     def draw_grid(self):
         for x in range(FIELD_W):
             for y in range(FIELD_H):
                 pg.draw.rect(self.app.screen, 'black', (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
 
     def update(self):
-        self.tetromino.update()
+        if self.app.anim_trigger:
+            self.tetromino.update()
         self.sprite_group.update()
 
     def draw(self):
