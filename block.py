ecdc37307a4cfac3ac2f004feb096860f091eb1e
Your Name <you@example.com>
2023-08-17 16:28:14 +0530

    Added special effects for full line(s)

--- block.py
+++ block.py
@@ -1,3 +1,5 @@
+import random
+
 from settings import *
 
 
@@ -14,9 +16,30 @@ class Block(pg.sprite.Sprite):
         self.rect.topleft = self.pos * TILE_SIZE
         self.alive = True
 
+        self.sfx_image = self.image.copy()
+        self.sfx_image.set_alpha(110)
+        self.sfx_speed = random.uniform(0.2, 0.6)
+        self.sfx_cycles = random.randrange(6, 8)
+        self.cycle_counter = 0
+
+    def sfx_end_time(self):
+        if self.tetromino.tetris.app.anim_trigger:
+            self.cycle_counter += 1
+            if self.cycle_counter > self.sfx_cycles:
+                self.cycle_counter = 0
+                return True
+
+    def sfx_run(self):
+        self.image = self.sfx_image
+        self.pos.y -= self.sfx_speed
+        self.image = pg.transform.rotate(self.image, pg.time.get_ticks() * self.sfx_speed)
+
     def is_alive(self):
         if not self.alive:
-            self.kill()
+            if not self.sfx_end_time():
+                self.sfx_run()
+            else:
+                self.kill()
 
     def rotate(self, pivot_pos):
         translated = self.pos - pivot_pos
