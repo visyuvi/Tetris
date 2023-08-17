c312c7df1b085c532727c74e3c9516842169d825
Your Name <you@example.com>
2023-08-17 10:07:57 +0530

    Created grid via Tetris class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pygame.event
-
 from settings import *
+from tetris import Tetris
 import sys
 
 
@@ -10,12 +9,16 @@ class App:
         pg.display.set_caption('Tetris')
         self.screen = pg.display.set_mode(FIELD_RES)
         self.clock = pg.time.Clock()
+        self.tetris = Tetris(self)
 
     def update(self):
+        self.tetris.update()
         self.clock.tick(FPS)
 
     def draw(self):
         self.screen.fill(color=FIELD_COLOR)
+        self.tetris.draw()
+        pg.display.flip()
 
     def check_events(self):
         for event in pg.event.get():
